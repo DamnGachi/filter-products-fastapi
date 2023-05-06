e87f38e70e4a15fe6f41ac777afc2090deb436e5
YOUR_NAME <YOUR_EMAIL>
2023-05-06 16:11:10 +0300

    123

---
+++ crud.py
@@ -0,0 +1,104 @@
+import re
+from slugify import slugify
+from pymongo import UpdateOne
+
+
+def update_sku(my_collection):
+    my_collection.update_many(
+        {"category": {"$in": ["одежда", "обувь", "сумки"]}},
+        [
+            {
+                "$set": {
+                    "sku": {
+                        "$regexFind": {
+                            "input": "$sku",
+                            "regex": "\\d+"
+                        }
+                    }
+                },
+                "$set": {
+                    "leftovers": [{"size": "U", "count": {"$sum": "$leftovers.count"}, "price": "$price"}]
+                }
+            }
+
+        ]
+    )
+    return {"message": "Цены и артиклы на все товары успешно присвоены"}
+
+
+def remove_color_from_category(my_collection):
+    my_collection.update_many(
+        {"size_table_type": {
+            "$in": ["парфюм", "парфюмерия", "Парфюм", "Парфюмерия"]}},
+        [{"$set": {
+            "color": {
+                "$regexFind": {
+                    "input": "$color",
+                    "regex": "/([^\s\/]+)$/",
+                    "options": "i"
+                }
+            }
+        }}]
+    )
+    return {"message": "Удалены все цвета из парфюмерии"}
+
+
+def crud_update_brand(my_collection):
+    products = my_collection.find({
+        "brand": {"$exists": True},
+        "color": {"$exists": True},
+        "size_table_type": {"$ne": "Парфюмерия"},
+        "root_category": {"$ne": "Косметика"},
+    }).limit(100)
+
+    for product in products:
+        print(product)
+        name = product["brand"]
+        color_code, color_name = product["color"].split(
+            "/") if "/" in product["color"] else (product["color"], "")
+        sku = product["sku"]
+
+        slug = slugify(f"{name} {color_code} {color_name} {sku}")
+
+        my_collection.update_one(
+            {"_id": product["_id"]},
+            {"$set": {"brand": {"name": name, "slug": slug, "color_name": color_name}}}
+        )
+
+    return {"message": "Бренды успешно обновлены"}
+
+
+def set_data_price(my_collection):
+
+    items = my_collection.find().limit(100)
+
+    for item in items:
+        if item.get("discount_price", 0) > 0:
+            item["price"] = item["discount_price"]
+        else:
+            item["discount_price"] = 0
+
+        if item["discount_price"] > item["price"] or item["price"] == item["discount_price"]:
+            item["price"] = item["discount_price"]
+            item["discount_price"] = 0
+
+        my_collection.update_one({"_id": item["_id"]}, {"$set": item})
+
+    return {"message": "Цены на все товары успешно присвоены"}
+
+
+def get_category_slug(category_name):
+    return slugify(category_name)
+
+
+def update_categories(my_collection):
+
+    categories = my_collection.distinct("root_category")
+
+    for category in categories:
+        slug = slugify(category)
+        category_entity = {"name": category, "slug": slug}
+        my_collection.update_many({"root_category": category}, {
+                                  "$set": {"category_entity": category_entity}})
+
+    return {"message": "Categories updated successfully"}
--- main.py
+++ main.py
@@ -1,28 +1,11 @@
+from enum import Enum
 import json
+from typing import Union
 from fastapi import FastAPI, File, HTTPException, UploadFile
+from crud import remove_color_from_category, crud_update_brand, update_categories, update_sku, set_data_price
 from database import my_collection
 
 app = FastAPI()
-
-
-@app.put("/set_price")
-async def set_price():
-
-    items = my_collection.find()
-
-    for item in items:
-        if item.get("discount_price", 0) > 0:
-            item["price"] = item["discount_price"]
-        else:
-            item["discount_price"] = 0
-
-        if item["discount_price"] > item["price"] or item["price"] == item["discount_price"]:
-            item["price"] = item["discount_price"]
-            item["discount_price"] = 0
-
-        my_collection.update_one({"_id": item["_id"]}, {"$set": item})
-
-    return {"message": "Цены на все товары успешно присвоены"}
 
 
 @app.post("/upload")
@@ -33,17 +16,26 @@ async def upload_data(data: UploadFile = File(...)):
     return {"status": "Data uploaded successfully!"}
 
 
+@app.put('/filter')
+async def product_filter():
+    update_sku(my_collection)
+    remove_color_from_category(my_collection)
+    update_categories(my_collection)
+    crud_update_brand(my_collection)
+    set_data_price(my_collection)
+    return {"message": "All functions finished successfully"}
+
+
 @app.get("/data_item")
 async def get_data(title: str,):
     query = {"title": title}
     projection = {"_id": 0}
 
-    results = my_collection.find(query, projection).limit(20)
+    results = my_collection.find(query, projection).limit(100)
 
     data = []
     for result in results:
         data.append(result)
-        print(result)
 
     return {"data": data}
 
@@ -55,10 +47,11 @@ async def get_data_price(min_price: int, max_price: int):
             status_code=302, detail="Min price should be lower than or equal to max price")
 
     query = {"leftovers": {"$elemMatch": {
-        "price": {"$gte": min_price, "$lte": max_price}}}}
+        "price": {"$gte": min_price, "$lte": max_price}}}
+    }
     projection = {"_id": 0}
 
-    results = my_collection.find(query, projection).limit(20)
+    results = my_collection.find(query, projection).limit(100)
 
     data = []
     for result in results:
@@ -67,13 +60,13 @@ async def get_data_price(min_price: int, max_price: int):
 
 
 @app.get("/data_brand")
-async def get_brands(brand: str | None = None):
+async def get_brands(brand: Union[str, None] = None):
     if brand is not None:
         query = {"brand": brand}
     else:
         query = {}
     projection = {"_id": 0}
-    results = my_collection.find(query, projection).sort("brand", 1).limit(120)
+    results = my_collection.find(query, projection).sort("brand", 1).limit(100)
     brands = []
     for result in results:
         if result["brand"] == "":
@@ -83,14 +76,14 @@ async def get_brands(brand: str | None = None):
 
 
 @app.get("/data_size")
-async def get_data_size(size: str | None = None):
+async def get_data_size(size: Union[str, None] = None):
     if size is not None:
         query = {"leftovers": {"size": size}}
     else:
         query = {}
     projection = {"_id": 0}
-    results = my_collection.find(query, projection).sort("size", 1).limit(20)
+    results = my_collection.find(query, projection).sort("size", 1).limit(100)
     sizes = []
     for result in results:
-        sizes.append(result["size"])
+        sizes.append(result)
     return {"size": sizes}
